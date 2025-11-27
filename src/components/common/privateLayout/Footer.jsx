a024c639a84fa1aacc23823dc3da0aef7756c3f3
Md. Rokonuzzaman <md.rokonuzzaman@Mds-MacBook-Air-2.local>
2025-11-27 15:04:13 +0600

    basic home page fina tune and SEO meta added

--- src/components/common/privateLayout/Footer.jsx
+++ src/components/common/privateLayout/Footer.jsx
@@ -7,21 +7,21 @@ const Footer = () => {
         <div className="footer-content space-y-6">
           <div className="footer-brand mx-auto space-y-6">
             <div className="logo ">
-              <img src="/assets/logo/LogoWhite.png" alt="Virtis AI" className="w-[120px] h-[40px] mx-auto" />
+              <img src="/assets/logo/Virtix_AI_Logo_White.png" alt="Virtis AI" className="h-[40px] mx-auto" />
             </div>
             <p className="footer-description text-center">
-              Unleash enterprise-grade AI agents to instantly engage, qualify, & convert inbound leads into pipeline & revenue.
+              Virtix AI is a conversation-first support platform that lets SMEs and enterprises handle chat, complaints, bookings, orders, and leads with AI agents – without hiring a big support team.
             </p>
           </div>
           <div className="flex flex-row gap-6 justify-center items-center ">
             <Link href="/" className="footer-link">Home</Link>
-            <Link href="/" className="footer-link">Feature</Link>
-            <Link href="/" className="footer-link">Price</Link>
-            <Link href="/" className="footer-link">Contact</Link>
+            <Link href="/features" className="footer-link">Feature</Link>
+            <Link href="/pricing" className="footer-link">Priceing</Link>
+            <Link href="/contact" className="footer-link">Contact</Link>
           </div>
         </div>
         <div className="footer-bottom">
-          <p>&copy; 2024 Virtis AI. All rights reserved.</p>
+          <p>&copy; 2024 Virtix AI. All rights reserved.</p>
         </div>
       </div>
     </footer>
